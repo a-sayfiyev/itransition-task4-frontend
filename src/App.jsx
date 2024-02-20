de4b4d126f3d9be0fe5882ddef033fc6a39dfc21
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-20 15:53:23 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import User from "./pages/User/User";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Regiser";
+import Landing from "./pages/Landing/Landing";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
