65f6bdd09a7a4f0c98c78c6e34ddaabc31b1ce76
a-sayfiyev <akobirsayfiyev27@gmail.com>
2024-02-21 12:03:13 +0500

    Update Nav with its styles

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     <div>
       <nav className="px-5 d-flex justify-content-between align-items-center ">
         <div className="logo ">
-          <h3 className="text-white fw-bold fs-2 m-0 ">userManagementPro</h3>
+          <h3 className="text-white fw-bold fs-2 m-0 ">ManageUs</h3>
         </div>
         <div className="links d-flex gap-3">
           <Link className="link-btn" to="/">
